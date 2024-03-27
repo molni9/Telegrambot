4690182e49485ab3bf00e5c11436378a82a6bf1a
molni9 <114047164+molni9@users.noreply.github.com>
2024-03-27 22:49:55 +0500

    dev-menu

--- src/main/java/com/example/molni9boot/service/Telegramboot.java
+++ src/main/java/com/example/molni9boot/service/Telegramboot.java
@@ -3,15 +3,38 @@ package com.example.molni9boot.service;
 import com.example.molni9boot.Config.BotConfig;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
+import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeDefault;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class Telegramboot extends TelegramLongPollingBot {
     final BotConfig config;
+
+    static final String help_Text = "В общем в этом боту вы можете бранировать себе место для стрижки в этом боту есть команды такие как:\n\n"
+            + "Еще в разроботке";
+
+
     public Telegramboot(BotConfig config){
         this.config = config;
+        List<BotCommand> listofCommands = new ArrayList();
+        listofCommands.add(new BotCommand("/start","get a welcom massage"));
+        listofCommands.add(new BotCommand("/help","Помощь тебе"));
+        listofCommands.add(new BotCommand("mydata","get your data store"));
+        listofCommands.add(new BotCommand("/deletedata","Убрать тебя нахуй твои данные"));
+        listofCommands.add(new BotCommand("/settings","Поменять настройки"));
+        try {
+            this.execute(new SetMyCommands(listofCommands, new BotCommandScopeDefault(), null));
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
+
     }
     @Override
     public String getBotUsername() {
@@ -32,6 +55,9 @@ public class Telegramboot extends TelegramLongPollingBot {
             switch (massageText) {
                 case "/start":
                     startCommandReceived(chatId, update.getMessage().getChat().getFirstName() );
+                    break;
+                case "/help":
+                    sendMassage(chatId, help_Text);
                     break;
                 default: sendMassage(chatId,"ПОШЕЛ НАХУЙ");
 
